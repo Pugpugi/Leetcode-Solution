dc809e899b07536e5a9dbb2f2a8132850ac66f9b
Mahinul Islam <136072243+Mahin-111@users.noreply.github.com>
2024-06-14 14:42:51 +0600

    Create Contains_duplicates.java

---
+++ Java/Arrays and Hashings/Contains_duplicates.java
@@ -0,0 +1,14 @@
+/*
+here I created a HashSet where all the integers will be stored without the duplicate then started a 
+loop and in the loop, I used an if statement to find a number that doesn't add to the set if it's true then it'll return true 
+or return false.
+*/
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       HashSet<Integer> hashset  = new  HashSet<Integer>();
+       for (int a: nums){
+            if (!hashset.add(a)){return true;} 
+       }
+       return false;
+    }
+}
