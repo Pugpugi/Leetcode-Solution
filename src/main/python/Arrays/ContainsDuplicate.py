689f760e3663a5722359d2440f523782d74a0e83
Mahinul Islam <136072243+Pugpugi@users.noreply.github.com>
2024-07-08 23:41:52 +0600

    added explanation

--- src/main/python/Arrays/ContainsDuplicate.py
+++ src/main/python/Arrays/ContainsDuplicate.py
@@ -1,8 +1,11 @@
+# to find if the code contains any duplicate I went through all the numbers and returned True if it's in the set else added in the set
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         seen = set()
         for num in nums:
             if num in seen:
                 return True
-            seen.add(num)
+            else:
+                seen.add(num)
         return False
